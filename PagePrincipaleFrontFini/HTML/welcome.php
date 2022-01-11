0fbb46c98fff0c67d9af42e8f620125372ce681c
Fixr0104 <aymentaouil123@gmail.com>
2022-01-11 19:17:53 +0100

    Ameliorations

--- PagePrincipaleFrontFini/HTML/welcome.php
+++ PagePrincipaleFrontFini/HTML/welcome.php
@@ -15,18 +15,20 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta charset="UTF-8">
     <title>Welcome</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/welcome.css">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet" />
 </head>
 <body>
     <?php include("header_recette.php") ?>
-    <h1 class="my-5">Bonjour, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenue sur notre site.</h1>
-    <p>
-        <a href="reset-password.php" class="btn ">Réinitialiser votre mot de passe</a>
-        <a href="logout.php" class="btn  ml-3">Déconnexion</a>
-    </p>
+    <div class="wrap-hello">
+        <h1 class="my-5">Bonjour, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenue sur notre site.</h1>
+        <div>
+            <a href="reset-password.php" class="btn fix">Réinitialiser votre mot de passe</a>
+            <a href="logout.php" class="btn  ml-3 fix">Déconnexion</a>
+        </div>
+    </div>
     <?php include("footer.php") ?>
 </body>
 </html>
